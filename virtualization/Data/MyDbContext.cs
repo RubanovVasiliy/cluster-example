b1d14999d045e4be4b8448fc80ed737d98b2f7f2
Rubanov Vasiliy <rubanovas2005@gmail.com>
2023-03-11 14:43:35 +0700

    fix db connection

--- virtualization/Data/MyDbContext.cs
+++ virtualization/Data/MyDbContext.cs
@@ -2,21 +2,26 @@ using Microsoft.EntityFrameworkCore;
 
 namespace virtualization.Data;
 
-public class MyDbContext: DbContext
+
+public class MyDbContext : DbContext
 {
     public MyDbContext(DbContextOptions<MyDbContext> options) : base(options)
     {
         Database.EnsureCreated();
     }
-    
-    // protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-    // {
-    //     var POSTGRES_USER = Environment.GetEnvironmentVariable("POSTGRES_USER");
-    //     var POSTGRES_PASSWORD = Environment.GetEnvironmentVariable("POSTGRES_PASSWORD");
-    //     var POSTGRES_DB = Environment.GetEnvironmentVariable("POSTGRES_DB");
-    //     Console.WriteLine(POSTGRES_PASSWORD + "  ++++++++++++++++");
-    //     optionsBuilder.UseNpgsql($"Host=db;Port=5432;Database={POSTGRES_DB};Username={POSTGRES_USER};Password={POSTGRES_PASSWORD};");
-    // }
 
-    public DbSet<User> Users => Set<User>();   
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        var POSTGRES_USER = Environment.GetEnvironmentVariable("POSTGRES_USER");
+        var POSTGRES_PASSWORD = Environment.GetEnvironmentVariable("POSTGRES_PASSWORD");
+        var POSTGRES_DB = Environment.GetEnvironmentVariable("POSTGRES_DB");
+        var POSTGRES_HOST = Environment.GetEnvironmentVariable("POSTGRES_HOST");
+        var POSTGRES_PORT = Environment.GetEnvironmentVariable("POSTGRES_PORT");
+        
+        optionsBuilder.UseNpgsql(
+            $"Host={POSTGRES_HOST};Port={POSTGRES_PORT};Database={POSTGRES_DB};Username={POSTGRES_USER};Password={POSTGRES_PASSWORD};"
+        );
+    }
+
+    public DbSet<User> Users => Set<User>();
 }
